d01b5753406ace680cc8fb99c22a2c995183d452
Ozias Gonet <oziasgonet@gmail.com>
2017-11-04 13:24:33 -0400

    connected with seans work

--- demoji-cfg.py
+++ demoji-cfg.py
@@ -1,28 +1,29 @@
 from nltk.parse.generate import generate
 from nltk import CFG
 import itertools
+import emojipaths as ep
 
 # N
 # V
 # A
 # S
 
-l = [{'N': ['boy', 'man', 'person'], 'V': ['boyed', 'made'], 'A': ['boyish', 'manly', 'masculine'], 'S': ['manly']},
-{'N': ['dog', 'pet', 'friend'], 'V': ['dogly'], 'A': ['doggy'], 'S': ['doggly']},
-{'N': ['runner'], 'V': ['run', 'exercise'], 'A': ['athletic'], 'S': ['athletically']},
-{'N': [], 'V': [], 'A': ['strange'], 'S': ['quickly', 'speedily', 'hastily']},
-{'N': ['cat'], 'V': [], 'A': [''], 'S': ['', '', '']},
-     ]
-test_clauses = [[('ANVS', "The boy runs"), ('ANVS', "The man runs"), ('ANVS', "The boy exercises")], [('ANVS',"the kid reads the book"),('ANVS',"the student reads the book"),('ANVS',"the kid reads an article")],
-                [('N',"the dog"),('N',"the pet"),('N',"the friend")]]
-perm = {1: ['N'], 2: ['AN', 'NV'], 3:['ANV', 'NVS'], 4: ['ANVS']}
+# l = [{'N': ['boy', 'man', 'person'], 'V': ['boyed', 'made'], 'A': ['boyish', 'manly', 'masculine'], 'S': ['manly']},
+# {'N': ['dog', 'pet', 'friend'], 'V': ['dogly'], 'A': ['doggy'], 'S': ['doggly']},
+# {'N': ['runner'], 'V': ['run', 'exercise'], 'A': ['athletic'], 'S': ['athletically']},
+# {'N': [], 'V': [], 'A': ['strange'], 'S': ['quickly', 'speedily', 'hastily']},
+# {'N': ['cat'], 'V': [], 'A': [''], 'S': ['', '', '']},
+#      ]
+# test_clauses = [[('ANVS', "The boy runs"), ('ANVS', "The man runs"), ('ANVS', "The boy exercises")], [('ANVS',"the kid reads the book"),('ANVS',"the student reads the book"),('ANVS',"the kid reads an article")],
+#                 [('N',"the dog"),('N',"the pet"),('N',"the friend")]]
+perm = {1: ['a'], 2: ['an', 'nv'], 3:['anv', 'nvr'], 4: ['anvr']}
 
 grammar = """
 Se -> NP VP
 NP -> Det AP
 Det -> 'The'
-AP -> A N
-VP -> V S
+AP -> a n
+VP -> v r
 """
 
 # combine_grammar = """
@@ -46,7 +47,8 @@ def gen(l):
         sequences += [gen_clause(l)]
 
     generated_strs = build_cfg_strings(sequences)
-    return generated_strs
+    #print(generated_strs)
+    return clause_perms(generated_strs)
 
 def build_cfg_strings(sequences):
     seq_list = []
@@ -55,7 +57,7 @@ def build_cfg_strings(sequences):
         emoji_seq = []
         for tuple in seq[key]:
             local_grammar = grammar
-            used_pos = {'N', 'A', 'V', 'S'}
+            used_pos = {'n', 'a', 'v', 'r'}
             for index, word in enumerate(tuple):
                 local_grammar += "{} -> '{}'\n".format(key[index], word)
                 used_pos.remove(key[index])
@@ -89,7 +91,7 @@ def combine_clauses(clause, tup2):
     # tup2 could be either another NV or jut a N.
     ret = clause
     new_clause = tup2[1]
-    verb = ('V' in tup2[0])
+    verb = ('v' in tup2[0])
     if ret is "":
         # This is our first clause.
         ret += new_clause
@@ -108,11 +110,14 @@ def gen_clause(l):
     # At a pos_string in cfg_values, there exists a list of lists of words in that POS_String order.
     # Example output:
     # {"NV": [['dog', 'run'], ['pet', 'exercise']]}
-    cfg_values = {}
+    cfg_values = {} #{'a':[], 'n': [], 'v': [], 'r': []}
     for pos_string in perm[len(l)]:
         # a is a list of lists of possible words per POS. this will be turned into a permutation lists.
         a = []
         for emoji_index in range(len(pos_string)):
+            pos = pos_string[emoji_index]
+            if pos not in l[emoji_index]:
+                l[emoji_index][pos] = []
             a.append(l[emoji_index][pos_string[emoji_index]])
         pos_perms = list(itertools.product(*a))
         cfg_values[pos_string] = pos_perms
@@ -121,6 +126,10 @@ def gen_clause(l):
 
 
 #print(gen(l))
-gen(l)
+#gen(l)
 #for sentence in generate(CFG.fromstring(grammar), n=100):
 #    print(' '.join(sentence))
+
+emoji_str = "\U0001F947\U0001F384\U0001F1EC\U0001F1EA\U0001F317"
+l = ep.translate_emoji_string(emoji_str)
+print(gen(l))
